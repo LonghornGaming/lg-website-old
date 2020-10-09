787a5505bd547b726bd075e9f7699e96d3687298
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2020-10-09 15:30:50 -0500

    mongodb fixes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  req.app.locals.db.collection('listingAndReviews').findOne('').then(result => {
+    console.log(result);
+  });
 });
 
 module.exports = router;
