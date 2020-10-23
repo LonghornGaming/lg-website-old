73b2ae0213b92a201e674606fb083b20c6a50c65
Colette Montminy <cmontminy@utexas.edu>
2020-10-23 16:37:40 -0500

    move sneaky stuff

--- react-app/src/Components/Events.js
+++ react-app/src/Components/Events.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Calendar from '@ericz1803/react-google-calendar';
-
-
-const API_KEY = "AIzaSyDWa_GxfQMPx3Ice2U7p8Si3W8Ajzar4OA";
-const CALENDAR_ID = "longhorngaming.gg_biorbbnr3sg9jciq876j2vsnss@group.calendar.google.com";
+const { calKey, calID } = require('../keys.js');
 
 function Events() {
     return (
         <div>
             <h1>Welcome to LG's Events Page!</h1>
-            <Calendar apiKey={API_KEY} calendarId={CALENDAR_ID} />
+            <Calendar apiKey={calKey} calendarId={calID} />
         </div>
     )
 }
