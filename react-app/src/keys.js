18d38b12ab5d8f619dfd809e7ecfecdae6a5a802
Colette Montminy <cmontminy@utexas.edu>
2020-10-23 16:49:08 -0500

    move keys file

--- react-app/src/keys.js
+++ react-app/src/keys.js
@@ -1,1 +1,8 @@
-../../keys.js
+const mongoKey = 'mongodb+srv://lgwebdev:kTMNQiSaq2PLTM7v@chad.l1dd7.mongodb.net/sample_airbnb?retryWrites=true&w=majority';
+const calKey   = 'AIzaSyDWa_GxfQMPx3Ice2U7p8Si3W8Ajzar4OA';
+const calID    = 'longhorngaming.gg_biorbbnr3sg9jciq876j2vsnss@group.calendar.google.com';
+module.exports = {
+    mongoKey,
+    calKey,
+    calID
+}
