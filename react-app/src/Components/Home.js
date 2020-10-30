ecffc975927677e3a1bd094466b3cece15702374
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2020-10-30 15:10:05 -0500

    updated home page

--- react-app/src/Components/Home.js
+++ react-app/src/Components/Home.js
@@ -1,8 +1,56 @@
 import React from 'react';
 
+import Calendar from '@ericz1803/react-google-calendar';
+
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Home1 from "../assets/home1.jpg";
+import Home2 from "../assets/home2.jpg";
+import Home3 from "../assets/home3.jpg";
+
+const { calKey, calID } = require('../keys.js');
+
 function Home() {
     return (
-        <h1>Welcome to LG's Home Page!</h1>
+        <div>
+            <h2>Get Involved</h2>
+            <p>There are so many ways to get involved with LG! Check out how below:</p>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col>
+                        <Image src={Home1} width="100%" min-height="185" rounded/>
+                        <h3>Community Events</h3>
+                        <p type="text">
+                            Interested in tournaments, LAN parties, casual events, and more?
+                            Check out our list of events here!
+                        </p>    
+                    </Col>
+                    <Col>
+                        <img src={Home2}  width="100%" min-height="185" rounded/>
+                        <h3>Competitive Teams</h3>
+                        <p type="text">
+                            Show the world what you and LG are made of by competing
+                            in tournaments for whatever game you want.
+                        </p>  
+                    </Col>
+                    <Col>
+                        <img src={Home3}  width="100%" min-height="185" rounded/>
+                        <h3>Learn More</h3>
+                        <p type="text">
+                            Connect with us on our social media and join our community on Hornslink
+                            by clicking the button below!
+                        </p>  
+                    </Col>
+                </Row>
+            </Container>
+
+            <h2>Upcoming Events</h2>
+            <Calendar apiKey={calKey} calendarId={calID} />
+
+        </div>
     )
 }
 
