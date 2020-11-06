e5ab7c4e513e79805738a3a3ce968e582e6038ad
Rushi <patelrushi1023@gmail.com>
2020-11-06 14:38:26 -0600

    finished cards for esports page

--- react-app/src/Components/ESports.js
+++ react-app/src/Components/ESports.js
@@ -75,6 +75,103 @@ function ESports() {
                     </Card.Body>
                 </Card>
             </CardDeck>
+
+            <br>
+            </br>
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src={images.madden}/>
+                    <Card.Body>
+                        <Card.Title>Madden 21</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.nba2k}/>
+                    <Card.Body>
+                        <Card.Title>NBA 2K21</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.osu}/>
+                    <Card.Body>
+                        <Card.Title>osu!</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.overwatch}/>
+                    <Card.Body>
+                        <Card.Title>Overwatch</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.pubg}/>
+                    <Card.Body>
+                        <Card.Title>PlayerUnknown's Battlegrounds</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+
+            <br>
+            </br>
+
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src={images.rainbow6}/>
+                    <Card.Body>
+                        <Card.Title>Rainbow Six Siege</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.rocketleague}/>
+                    <Card.Body>
+                        <Card.Title>Rocket League</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.smashmelee}/>
+                    <Card.Body>
+                        <Card.Title>Super Smash Bros. Melee</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.smashultimate}/>
+                    <Card.Body>
+                        <Card.Title>Super Smash Bros. Ultimate</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.starcraft2}/>
+                    <Card.Body>
+                        <Card.Title>Starcraft 2</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+
+            <br>
+            </br>
+
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src={images.streetfighter5}/>
+                    <Card.Body>
+                        <Card.Title>Street Fighter V</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.undernight}/>
+                    <Card.Body>
+                        <Card.Title>Under Night In-Birth</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={images.valorant}/>
+                    <Card.Body>
+                        <Card.Title>Valorant</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+
+
         </div>
     )
 }
--- react-app/src/assets/images.js
+++ react-app/src/assets/images.js
@@ -8,5 +8,21 @@ import fortnite from './fortnite.jpeg'
 import hearthstone from './hearthstone.jpg'
 import herosofthestorm from './heroesofthestorm.png'
 import leagueoflegends from './leagueoflegends.png'
+import madden from './madden.jpeg'
+import nba2k from './nba2k.jpg'
+import osu from './osu.png'
+import overwatch from './overwatch.png'
+import pubg from './pubg.jpg'
+import rainbow6 from './rainbow6.jpg'
+import rocketleague from './rocketleague.png'
+import smashmelee from './smashmelee.png'
+import smashultimate from './smashultimate.png'
+import starcraft2 from './starcraft2.png'
+import streetfighter5 from './streetfighter5.png'
+import undernight from './undernight.png'
+import valorant from './valorant.png'
+export default {
+    apexLegends, beatsaber, brawlstars, callofduty, csgo, dota, fortnite, hearthstone, herosofthestorm, leagueoflegends, madden, nba2k,
+    osu, overwatch, pubg, rainbow6, rocketleague, smashmelee, smashultimate, starcraft2, streetfighter5, undernight, valorant
+}
 
-export default {apexLegends, beatsaber, brawlstars, callofduty, csgo, dota, fortnite, hearthstone, herosofthestorm, leagueoflegends}
