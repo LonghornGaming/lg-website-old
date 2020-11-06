8a843489f0344d94edc26580d9b8000b097bdec6
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2020-11-06 14:15:07 -0600

    updated home page to use images.js

--- react-app/src/assets/images.js
+++ react-app/src/assets/images.js
@@ -8,5 +8,10 @@ import fortnite from './fortnite.jpeg'
 import hearthstone from './hearthstone.jpg'
 import herosofthestorm from './heroesofthestorm.png'
 import leagueoflegends from './leagueoflegends.png'
+import home1 from './home1.jpg'
+import home2 from './home2.jpg'
+import home3 from './home3.jpg'
+import utlandscape from './ut.jpg'
 
-export default {apexLegends, beatsaber, brawlstars, callofduty, csgo, dota, fortnite, hearthstone, herosofthestorm, leagueoflegends}
+export default {apexLegends, beatsaber, brawlstars, callofduty, csgo, dota, fortnite, hearthstone, herosofthestorm, leagueoflegends,
+home1, home2, home3, utlandscape}
