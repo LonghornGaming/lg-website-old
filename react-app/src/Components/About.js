45d1dfe7c0d01ed7bef159ea37372169425134bd
Colette Montminy <cmontminy@utexas.edu>
2020-11-06 15:49:46 -0600

    try to make the about page better

--- react-app/src/Components/About.js
+++ react-app/src/Components/About.js
@@ -1,56 +1,313 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
-import testImg from '../assets/dog.jpg';
+import testImg from "../assets/dog.jpg";
 
 const cardStyle = {
-    height: '300px',
+    height: "200px",
+};
+
+const rowStyle = {
+    paddingBottom: "20px",
 };
 
 function About() {
     return (
-        <CardDeck>
-            <Card>
-                <Card.Img style={cardStyle} variant="top" src={testImg}/>
-                <Card.Body>
-                    <Card.Title>Hunter</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">President</Card.Subtitle>
-                    <Card.Text>
-                        Hunter is a senior Archaeology major. His favorite games
-                        are fetch and belly rubs.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Img style={cardStyle} variant="top" src={testImg}/>
-                <Card.Body>
-                    <Card.Title>Hunter</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">President</Card.Subtitle>
-                    <Card.Text>
-                        Hunter is a senior Archaeology major. His favorite games
-                        are fetch and belly rubs.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Img style={cardStyle} variant="top" src={testImg}/>
-                <Card.Body>
-                    <Card.Title>Hunter</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">President</Card.Subtitle>
-                    <Card.Text>
-                        Hunter is a senior Archaeology major. His favorite games
-                        are fetch and belly rubs.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-        </CardDeck>
+        <Container>
+            <Row style={rowStyle}>
+                <h2>
+                    About LG
+                </h2>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Row>
+            <Row> Advisors </Row>
+            <Row style={rowStyle}>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+            <Row>Cabinet</Row>
+            <Row style={rowStyle}>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+            <Row>Directors</Row>
+            <Row style={rowStyle}>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+                <Col><Card>
+                        <Card.Img
+                            style={cardStyle}
+                            variant="top"
+                            src={testImg}
+                        />
+                        <Card.Body>
+                            <Card.Title>Hunter</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                President
+                            </Card.Subtitle>
+                            <Card.Text>
+                                Hunter is a senior Archaeology major. His
+                                favorite games are fetch and belly rubs.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card></Col>
+            </Row>
+
+            <Row>Crew</Row>
+        </Container>
     );
 }
 
