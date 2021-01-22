e581a784ead715eac4974d2888b586f3bf4e4737
Colette Montminy <cmontminy@utexas.edu>
2021-01-22 13:52:17 -0600

    made the footer look a bit better

--- react-app/src/Components/Footer.js
+++ react-app/src/Components/Footer.js
@@ -4,9 +4,6 @@ import Row       from "react-bootstrap/Row";
 import Col       from "react-bootstrap/Col";
 import { Link }  from "react-router-dom";
 
-import logo      from "../assets/Orange_Logo-p-500.png";
-import Home      from "./Home";
-
 import { ReactComponent as Discord }   from "../assets/icons/discord.svg";
 import { ReactComponent as Facebook }  from "../assets/icons/facebook.svg";
 import { ReactComponent as Instagram } from "../assets/icons/instagram.svg";
@@ -17,8 +14,9 @@ import { ReactComponent as Steam }     from "../assets/icons/steam.svg";
 import { ReactComponent as Twitch }    from "../assets/icons/twitch.svg";
 import { ReactComponent as Beepo }     from "../assets/icons/beepo.svg";
 import { ReactComponent as Logo}       from "../assets/icons/logo.svg";
+import { ReactComponent as Reddit}     from "../assets/icons/reddit.svg";
 
-import { footerStyle } from "../public/style";
+import { footerStyle, centerStyle } from "../public/style";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Icon(props) {
@@ -32,183 +30,80 @@ function Icon(props) {
 function Footer() {
     return (
         <div style={footerStyle}>
-            <Container style={{paddingTop: "20px"}}>
-                <Row style={{display: 'flex', justifyContent: 'center'}}>
-                    <Link to="/home">
-                        <Icon
-                            url=""
-                            icon=<Logo height="40%" width="40%" />
-                        />
-                    </Link>
-                </Row>
-                <Row>
+
+            <Container style={{paddingTop: "80px", height: "200px"}}>
+                <Row className="justify-content-md-center">
                     <Col>
                         <Icon
                             url="https://www.discord.gg/longhorngaming"
-                            icon=<Discord height="55%" width="55%" />
+                            icon=<Discord />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://www.twitch.tv/utlonghorngaming"
-                            icon=<Twitch height="55%" width="55%" />
+                            icon=<Twitch />
                         />
                     </Col>
                     <Col>
                     <   Icon
                             url="https://www.youtube.com/longhorngaming"
-                            icon=<Youtube height="55%" width="55%" />
+                            icon=<Youtube />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://www.instagram.com/longhorngaming/"
-                            icon=<Instagram height="55%" width="55%" />
+                            icon=<Instagram />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="http://steamcommunity.com/groups/LonghornGaming"
-                            icon=<Steam height="55%" width="55%" />
+                            icon=<Steam />
+                        />
+                    </Col>
+                    <Col>
+                        <Link to="/home">
+                            <Icon
+                                url=""
+                                icon=<Logo />
+                            />
+                        </Link>
+                    </Col>
+                    <Col style={centerStyle}>
+                        <Icon
+                            url="https://www.reddit.com/r/LonghornGaming/"
+                            icon=<Reddit />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://twitter.com/longhorn_gaming"
-                            icon=<Twitter height="55%" width="55%" />
+                            icon=<Twitter />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://utexas.campuslabs.com/engage/organization/longhorngaming"
-                            icon=<Beepo height="55%" width="55%" />
+                            icon=<Beepo />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://www.facebook.com/ut.longhorn.gaming"
-                            icon=<Facebook height="55%" width="55%" />
+                            icon=<Facebook />
                         />
                     </Col>
                     <Col>
                         <Icon
                             url="https://www.linkedin.com/company/longhorn-gaming/"
-                            icon=<Linkedin height="55%" width="55%" />
+                            icon=<Linkedin />
                         />
                     </Col>
                 </Row>
             </Container>
 
-
-            {/* <Container>
-                <Row>
-                    <Col style={{paddingTop: "20px"}}>
-                        <Link to="/home">
-                            <img src={logo} width="20%" height="40%"></img>
-                        </Link>
-                        <br/>
-                        Copyright © Longhorn Gaming
-                    </Col>
-
-                    <Col>
-                        <h4>Join Our Communities</h4>
-                        <br/>
-                        <ul>
-                            <li>
-                                <a
-                                    href={
-                                        "https://discord.com/invite/longhorngaming"
-                                    }
-                                >
-                                    Discord
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={
-                                        "https://utexas.campuslabs.com/engage/organization/longhorngaming"
-                                    }
-                                >
-                                    HornsLink
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={
-                                        "http://steamcommunity.com/groups/LonghornGaming"
-                                    }
-                                >
-                                    Steam Group
-                                </a>
-                            </li>
-                        </ul>
-                    </Col>
-
-                    <Col>
-                        <h4>Connect With Us</h4>
-                        <br/>
-                        <ul>
-                            <li>
-                                <a
-                                    href={
-                                        "https://www.facebook.com/ut.longhorn.gaming"
-                                    }
-                                >
-                                    Facebook
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={
-                                        "https://www.instagram.com/longhorngaming/"
-                                    }
-                                >
-                                    Instagram
-                                </a>
-                            </li>
-                            <li>
-                                <a href={"https://twitter.com/longhorn_gaming"}>
-                                    Twitter
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={
-                                        "https://www.linkedin.com/company/longhorn-gaming/"
-                                    }
-                                >
-                                    LinkedIn
-                                </a>
-                            </li>
-                        </ul>
-                    </Col>
-
-                    <Col>
-                        <h4>Watch Our Content</h4>
-                        <br/>
-                        <ul>
-                            <li>
-                                <a
-                                    href={
-                                        "https://www.youtube.com/longhorngaming"
-                                    }
-                                >
-                                    Youtube
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={
-                                        "https://www.twitch.tv/utlonghorngaming"
-                                    }
-                                >
-                                    Twitch
-                                </a>
-                            </li>
-                        </ul>
-                    </Col>
-                </Row>
-            </Container> */}
         </div>
     );
 }
