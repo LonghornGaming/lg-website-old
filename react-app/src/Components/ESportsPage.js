92fc7fd6e632dc20b993d9ad172d1bb99ad4a085
John Nguyen <33184644+johnnguyen3196@users.noreply.github.com>
2021-03-26 12:57:56 -0500

    Esports skeleton done

--- react-app/src/Components/ESportsPage.js
+++ react-app/src/Components/ESportsPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios';
+import {Container, Row, Col, Card, CardDeck} from 'react-bootstrap';
 
 class ESportsPage extends React.Component{
     constructor(props) {
@@ -9,23 +10,119 @@ class ESportsPage extends React.Component{
         let foo = params.get('id');
         foo = './EsportsData/' + foo + '.json'
         this.state = { isLoading: true, id: foo, data: undefined };
-        console.log(this.state.id)
     }
 
     componentDidMount() {
         axios.get('./test.json').then(response => {
             this.setState({data:response.data, isLoading:false});
-            console.log(response.data);
         });
     }
 
+    achievementsToArray(){
+        let achievements = this.state.data.achievements;
+        let o = [];
+        achievements.forEach(achievement => {
+            o.push(<li>{achievement}</li>)
+        });
+        return o;
+    }
+
+    createDecks(players){
+        let decks = [];
+        for(let i = 0; i < players.length; i += 3) {
+            decks.push(this.createDeck(players.slice(i, i + 3)));
+        }
+        return decks;
+    }
+
+    createDeck(players){
+        let cards = [];
+        players.forEach(player => {
+           cards.push(this.createPlayer(player));
+        });
+        return <CardDeck>{cards}</CardDeck>
+    }
+
+    createPlayer(player){
+        return(
+            <Card style={{ width: '18rem', color:'white'}} bg={'dark'}>
+                <Card.Title>{player.name}</Card.Title>
+                <Card.Subtitle>{player.role}</Card.Subtitle>
+                <Card.Text>{player.description}</Card.Text>
+            </Card>
+        )
+    }
+
     render(){
+        const data = this.state.data;
         if(this.state.isLoading){
             return <div className="App">Loading...</div>;
         }
 
         return(
-            <div>Works</div>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>{data.name}</h1>
+                    </Col>
+
+                    <Col>
+                        <p>{data.description}</p>
+                    </Col>
+                </Row>
+
+                <br/>
+
+                <Row>
+                    <h3>Recent Achievements</h3>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <img style={{width:435, height:416}} src={data.photo}></img>
+                    </Col>
+
+                    <Col>
+                        <ul>
+                            {this.achievementsToArray()}
+                        </ul>
+                    </Col>
+                </Row>
+
+                <br/>
+
+                <Row>
+                    <h3>Players</h3>
+                </Row>
+
+                <Row>
+                    {this.createDecks(data.players)}
+                </Row>
+
+                <br/>
+
+                <Row>
+                    <Col>
+                        <h3>Tryouts Info</h3>
+                    </Col>
+
+                    <Col>
+                        <p>{data.tryout}</p>
+                    </Col>
+                </Row>
+
+                <br/>
+
+                <Row>
+                    <Col>
+                        <h3>Related Communities</h3>
+                    </Col>
+
+                    <Col>
+                        <p>{data.communities}</p>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
