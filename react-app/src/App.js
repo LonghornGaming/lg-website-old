96507c49f5238cfe4d9ce8dbe5ef09dc9565978d
Colette Montminy <cmontminy@utexas.edu>
2021-04-02 13:39:32 -0500

    nav bar go brr

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,60 +1,114 @@
-import React  from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import NavBar from 'react-bootstrap/Navbar';
-import Nav    from 'react-bootstrap/Nav';
+import React, {useState} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-import Blogs     from './Components/Blogs';
-import About    from './Components/About';
-import ESports from './Components/ESports';
-import Events from './Components/Events';
-import NotFound from './Components/NotFound';
-import Home     from './Components/Home';
-import Socials from './Components/Socials'
+import Blogs from "./Components/Blogs";
+import About from "./Components/About";
+import ESports from "./Components/ESports";
+import Events from "./Components/Events";
+import NotFound from "./Components/NotFound";
+import Home from "./Components/Home";
+import Socials from "./Components/Socials";
 import Blog from "./Components/Blog";
 import ESportsPage from "./Components/ESportsPage";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './assets/LGWhiteLogo.png';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/LGWhiteLogo.png";
 
-import Footer from './Components/Footer';
+import Footer from "./Components/Footer";
+
+function HoverControlledDropdown(props) {
+    const [isHovered, setIsHovered] = useState(false);
+    // const [isClicked, setIsClicked] = useState(false);
+
+    return (
+        <NavDropdown
+            {...props}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            show={isHovered}
+        />
+    );
+}
 
 function App() {
-  return (
+    return (
+        <Router>
+            <NavBar bg="dark" variant="dark">
+                <NavBar.Brand>
+                    {" "}
+                    <img src={logo} alt="LG Logo" width="35" height="30" />{" "}
+                </NavBar.Brand>
+                <Nav>
+                    <Nav.Link href="home">Home</Nav.Link>
+                    <Nav.Link href="about">About</Nav.Link>
+                    <HoverControlledDropdown href="eSports" title="eSports">
+                        <NavDropdown.Item href="about">
+                            Action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="events">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="socials">
+                            Something
+                        </NavDropdown.Item>
+                    </HoverControlledDropdown>
+                    <Nav.Link href="events">Events</Nav.Link>
+                    <Nav.Link href="blogs">Blogs</Nav.Link>
+                    <Nav.Link href="socials">Socials</Nav.Link>
+                    <Nav.Link
+                        href="https://discord.com/invite/longhorngaming"
+                        target="_blank"
+                    >
+                        Join Our Discord
+                    </Nav.Link>
+                </Nav>
+            </NavBar>
 
-    <Router>
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/esports">
+                    <ESports />
+                </Route>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+                <Route exact path="/events">
+                    <Events />
+                </Route>
+                <Route exact path="/blogs">
+                    <Blogs />
+                </Route>
+                <Route exact path="/socials">
+                    <Socials />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/404">
+                    <NotFound />
+                </Route>
+                <Route exact path="/blog/">
+                    <Blog />
+                </Route>
+                <Route exact path="/esport">
+                    <ESportsPage />
+                </Route>
+                <Redirect to="/404"></Redirect>
+            </Switch>
 
-      <NavBar bg='dark' variant='dark'>
-        <NavBar.Brand> <img src={logo} alt="LG Logo" width="35" height="30"/> </NavBar.Brand>
-        <Nav>
-          <Nav.Link href='home'>Home</Nav.Link>
-          <Nav.Link href='about'>About</Nav.Link>
-          <Nav.Link href='eSports'>eSports</Nav.Link>
-          <Nav.Link href='events'>Events</Nav.Link>
-          <Nav.Link href='blogs'>Blogs</Nav.Link>
-          <Nav.Link href='socials'>Socials</Nav.Link>    
-         <Nav.Link href='https://discord.com/invite/longhorngaming' target='_blank'>Join Our Discord</Nav.Link>
-        </Nav>
-      </NavBar>
-
-      <Switch>
-        <Route exact path="/about"><About /></Route>
-        <Route exact path="/esports"><ESports /></Route>
-        <Route exact path="/home"><Home /></Route>
-        <Route exact path="/events"><Events /></Route>
-        <Route exact path="/blogs"><Blogs /></Route>
-        <Route exact path="/socials"><Socials /></Route>
-        <Route exact path="/"><Home /></Route>
-        <Route exact path="/404"><NotFound /></Route>
-        <Route exact path='/blog/'><Blog/></Route>
-        <Route exact path='/esport'><ESportsPage/></Route>
-        <Redirect to="/404"></Redirect>
-      </Switch>
-
-      <Footer/>
-  </Router>
-
-  
-  )
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
