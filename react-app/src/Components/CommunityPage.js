f13a24e5825753aa79eaea6ccf92adeae68037bb
John Nguyen <33184644+johnnguyen3196@users.noreply.github.com>
2021-04-16 14:00:06 -0500

    Community setup

--- react-app/src/Components/CommunityPage.js
+++ react-app/src/Components/CommunityPage.js
@@ -2,55 +2,29 @@ import React from "react";
 import axios from 'axios';
 import {Container, Row, Col, Card, CardDeck} from 'react-bootstrap';
 
-class ESportsPage extends React.Component{
+class CommunityPage extends React.Component {
     constructor(props) {
         super(props);
         let search = window.location.search;
         let params = new URLSearchParams(search);
         let foo = params.get('id');
-        foo = './EsportsData/' + foo + '.json'
-        this.state = { isLoading: true, id: foo, data: undefined };
+        foo = './CommunityData/' + foo + '.json'
+        this.state = {isLoading: true, id: foo, data: undefined};
     }
 
     componentDidMount() {
         axios.get(this.state.id).then(response => {
-            this.setState({data:response.data, isLoading:false});
+            this.setState({data: response.data, isLoading: false});
         });
     }
 
-    achievementsToArray(){
+    achievementsToArray() {
         let achievements = this.state.data.achievements;
         let o = [];
         achievements.forEach(achievement => {
             o.push(<li>{achievement}</li>)
         });
         return o;
-    }
-
-    createDecks(players){
-        let decks = [];
-        for(let i = 0; i < players.length; i += 3) {
-            decks.push(this.createDeck(players.slice(i, i + 3)));
-        }
-        return decks;
-    }
-
-    createDeck(players){
-        let cards = [];
-        players.forEach(player => {
-           cards.push(this.createPlayer(player));
-        });
-        return <CardDeck>{cards}</CardDeck>
-    }
-
-    createPlayer(player){
-        return(
-            <Card style={{ width: '435px', height: "129px", color:'white', marginBottom: "28px", marginRight: "26px"}} bg={'dark'}>
-                <Card.Title style={{position: "relative", top: "16px", left: "30px", fontWeight: "700"}}>{player.name}</Card.Title>
-                <Card.Subtitle style={{position: "relative", top: "13px", left: "30px", fontSize: "18px"}}>{player.role}</Card.Subtitle>
-                <Card.Text style={{position: "relative", top:"16px", left: "30px", fontSize: "14px"}}>{player.description}</Card.Text>
-            </Card>
-        )
     }
 
     render(){
@@ -95,22 +69,6 @@ class ESportsPage extends React.Component{
                     <h3 style={{font: "Futura", fontWeight:"bold", fontSize: "28px", lineHeight: "36px", marginBottom: "16px"}}>Players</h3>
                 </Row>
 
-                <Row>
-                    {this.createDecks(data.players)}
-                </Row>
-
-                <br/>
-
-                <Row>
-                    <Col>
-                        <h3>Tryouts Info</h3>
-                    </Col>
-
-                    <Col>
-                        <p>{data.tryout}</p>
-                    </Col>
-                </Row>
-
                 <br/>
 
                 <Row>
@@ -127,4 +85,4 @@ class ESportsPage extends React.Component{
     }
 }
 
-export default ESportsPage
+export default CommunityPage;
